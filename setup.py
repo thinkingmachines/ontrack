8808802669e717c7cc59a1f51a108ecee32ceebb
Ray Dino <ray.e.dino@gmail.com>
2016-03-11 14:04:39 +0800

    Fixed dependencies

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 setup(name='ontrack',
       version='1.0',
       py_modules=['cosine_matcher', 'text_cleaners', 'string_searcher'],
-      requires=['numpy', 'pandas', 'nltk', 'sklearn'],
+      install_requires=['numpy', 'pandas', 'nltk', 'sklearn'],
       scripts=['scripts/download_nltk'])
