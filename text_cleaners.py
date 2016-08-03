45751e5c818f79c3d4b1b04ebc186fa905113f89
liangi <gil@thinkingmachin.es>
2016-08-03 08:18:58 +0800

    made stem(text) faster

--- text_cleaners.py
+++ text_cleaners.py
@@ -18,6 +18,7 @@ from nltk.corpus import stopwords
 from string import maketrans
 
 stopwords = set(['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', 'her', 'hers', 'herself', 'it', 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', 'should', 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', 'couldn', 'didn', 'doesn', 'hadn', 'hasn', 'haven', 'isn', 'ma', 'mightn', 'mustn', 'needn', 'shan', 'shouldn', 'wasn', 'weren', 'won', 'wouldn'])
+stemmer = PorterStemmer()
 
 def clean_punctuation(text):
     return ''.join([w if w.isalnum() else ' ' for w in text.lower()])
@@ -29,13 +30,11 @@ def remove_stop(text):
     no_stopwords = [word for word in text.split() if not word in stopwords]
     return no_stopwords
 
-def stem(tokens):
+def stem(text):
     '''
     Applies PorterStemmer to the tokens
     '''
-    stemmed = []
-    for item in tokens:
-        stemmed.append(PorterStemmer().stem(item))
+    stemmed = [stemmer.stem(word) for word in text]
     return stemmed
 
 def stringify(tokens):
