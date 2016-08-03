82f90a9f1e14e49a746b389a2313174dacf714e1
liangi <gil@thinkingmachin.es>
2016-08-03 08:06:50 +0800

    clean_punctuation uses alnum instead
    Pro: can handle unicode
    Con: not able to pinpoint exactly what was removed

--- text_cleaners.py
+++ text_cleaners.py
@@ -20,16 +20,7 @@ from string import maketrans
 stopwords = set(['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', 'her', 'hers', 'herself', 'it', 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', 'should', 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', 'couldn', 'didn', 'doesn', 'hadn', 'hasn', 'haven', 'isn', 'ma', 'mightn', 'mustn', 'needn', 'shan', 'shouldn', 'wasn', 'weren', 'won', 'wouldn'])
 
 def clean_punctuation(text):
-    #fixed_slashes = text.replace('/',' ')
-    fixed_slashes = text
-    custom_punc = '!"#$%&\'()*,.:;<=>?@[\]^_`{|}-~'
-    translate_table = maketrans(custom_punc, ''.join([' ' for i in range(
-        0, len(custom_punc))]))
-    try:
-        desc = fixed_slashes.lower().translate(translate_table)
-        return desc
-    except TypeError:
-        print('TypeError: Translate function does not accept unicode.')
+    return ''.join([w if w.isalnum() else ' ' for w in text.lower()])
 
 def remove_stop(text):
     '''
