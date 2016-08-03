324814591b4071b58cf5f576c7b2449b8bba37b0
liangi <gil@thinkingmachin.es>
2016-08-03 14:06:11 +0800

    allowed newer versions of scipy, reduce nltk
    no need to download the stopwords and punkt corpus since the text_cleaners was edited

--- setup.py
+++ setup.py
@@ -6,8 +6,4 @@ except ImportError:
 setup(name='ontrack',
       version='1.0',
       py_modules=['cosine_matcher', 'text_cleaners', 'string_searcher'],
-      install_requires=['numpy', 'pandas', 'nltk', 'scipy==0.16.1', 'scikit-learn'])
-
-import nltk
-nltk.download('stopwords')
-nltk.download('punkt')
+      install_requires=['numpy', 'pandas', 'nltk', 'scipy>=0.16.1', 'scikit-learn'])
