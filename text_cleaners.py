3e9acb5270e702bee4b94d76289088441f079dcb
liangi <gil@thinkingmachin.es>
2016-08-18 13:19:10 +0800

    added option to preserve certain characters

--- text_cleaners.py
+++ text_cleaners.py
@@ -7,7 +7,7 @@ from nltk.stem.porter import PorterStemmer
 stopwords = set(['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', 'her', 'hers', 'herself', 'it', 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', 'should', 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', 'couldn', 'didn', 'doesn', 'hadn', 'hasn', 'haven', 'isn', 'ma', 'mightn', 'mustn', 'needn', 'shan', 'shouldn', 'wasn', 'weren', 'won', 'wouldn'])
 stemmer = PorterStemmer()
 
-def clean_punctuation(text):
+def clean_punctuation(text, keepChars=''):
     """
     Converts to lowercase and replaces all non-alphanumeric with spaces.
 
@@ -18,7 +18,10 @@ def clean_punctuation(text):
     clean_punctuation('Rehabilitation/Reconstruction/Removal of Gravel')
     >>> ['rehabilitation', 'reconstruction', 'removal', 'of', 'gravel']
     """
-    no_punc = ''.join([w if w.isalnum() else ' ' for w in text.lower()])
+    if keepChars:
+        no_punc = ''.join([w if w.isalnum() or w in keepChars else ' ' for w in text.lower()])
+    else:
+        no_punc = ''.join([w if w.isalnum() else ' ' for w in text.lower()])
     return no_punc.split()
 
 def remove_stop(tokens):
@@ -32,7 +35,7 @@ def remove_stop(tokens):
     remove_stop(['rehabilitation', 'reconstruction', 'removal', 'of', 'gravel'])
     >>> ['rehabilitation', 'reconstruction', 'removal', 'gravel']
     """
-    no_stopwords = [word for word in tokens 
+    no_stopwords = [word for word in tokens
                     if (not word in stopwords) and (len(word) > 1)]
     return no_stopwords
 
