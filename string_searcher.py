4da2171cd0e7b8d445b974177df1a80609f4bd08
liangi <gil@thinkingmachin.es>
2016-11-14 13:30:49 +0800

    converted class to a function

--- string_searcher.py
+++ string_searcher.py
@@ -1,52 +1,25 @@
 """
 This class allows filtering of a dataframe based on whether or not a
 dataframe column value contains the target string.
-
-SAMPLE USAGE:
-searcher = StringSearcher()
-searcher.set_corpus('C:/Users/User/Desktop/myFile.csv')
-searcher.find('manila irrigation')
->>> manila north rd ...
 """
+
 
 from __future__ import print_function
 import pandas as pd
 
+def es_find(df, pattern, regex=False):
+    '''
+    df: The database to find the pattern in
+    pattern: A string to search for.
+    regex: Set to True to use pattern as a regex.
 
-class StringSearcher(object):
+    self.corpus must be a dataframe with a column whose name is specified
+    in the "on" argument.
 
-    def __init__(self):
-        self.corpus = None
-
-    def set_corpus(self, corpus):
-        """
-        Set the dataframe to use as a search space.
-
-        corpus: Path to CSV file or pandas dataframe.
-        """
-        if type(df_corpus) in [str, unicode]:
-            corpus = pd.read_csv(corpus)
-        self.corpus = corpus
-
-    def find(self, pattern, on = 'searchspace', regex=False, dfOutput=True):
-        """
-        pattern: A string to search for.
-        on: name of data frame column to use as search space.
-        regex: Set to True to use pattern as a regex.
-        dfOutput: set to False to output a dictionary instead of a pandas dataframe
-
-        self.corpus must be a dataframe with a column whose name is specified
-        in the "on" argument.
-
-        Returns a dictionary.
-        """
-        search = self.corpus[self.corpus[on].notnull()].astype(str)
-        found = search[search[on].str.contains(str(pattern),
-                                               case=False,
-                                               regex=regex)]
-        if dfOutput:
-            return found
-        return found.to_dict('list')
+    Returns the dfA row numbers of satisfactory rows in dfB.
+    '''
+    found = df[df.str.contains(str(pattern), case=False, regex=regex)].index
+    return found
 
 if __name__ == '__main__':
     pass
